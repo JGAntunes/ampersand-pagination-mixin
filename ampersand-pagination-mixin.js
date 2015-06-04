693c90b99db4e01c60270980f7faa1228b1b10c9
JGAntunes <j.goncalo.antunes@gmail.com>
2015-06-04 03:50:57 +0100

    solving some issues with the API

--- ampersand-pagination-mixin.js
+++ ampersand-pagination-mixin.js
@@ -2,17 +2,19 @@
 var assign = require('lodash.assign');
 
 module.exports ={
-  limit: 10,
-  skip: 0,
   fetchPage: function(options){
     options || (options = {remove: false, merge: false, add: true});
     options.data = options.data || {};
+    var data = this.data;
     if(options.reset){
-        options = {};
-        this.skip = 0;
+      options = {};
+      data.skip = 0;
     }
-    assign(this, options.data);
-    this.fetch(options);
-    this.skip += this.limit;
+    assign(data, options.data);
+    data.skip = data.skip ? data.skip : 0;
+    data.limit = data.limit ? data.limit : 10;
+    this.fetch(data);
+    data.skip += data.limit;
+    this.data = data;
   }
 };
